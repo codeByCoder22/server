dac69f0c2e1e82ef813c3d6aec4d7b297b4fa518
WuQiang <wuqiang5733@gmail.com>
2023-05-12 12:19:59 +0930

    04_08_Create column with websockets

--- src/controllers/columns.ts
+++ src/controllers/columns.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Response } from "express";
 import { ExpressRequestInterface } from "../types/expressRequest.interface";
 import ColumnModel from "../models/column";
+import { SocketEventsEnum } from "../types/socketEvents.enum";
+import { getErrorMessage } from "../helpers";
+import { Socket } from "../types/socket.interface";
+import { Server } from "socket.io";
 
 export const getColumns = async (
     req: ExpressRequestInterface,
@@ -15,5 +19,37 @@ export const getColumns = async (
         res.send(columns);
     } catch (err) {
         next(err);
+    }
+};
+
+export const createColumn = async (
+    io: Server,
+    socket: Socket,
+    data: { boardId: string; title: string }
+) => {
+    try {
+        if (!socket.user) {
+            socket.emit(
+                SocketEventsEnum.columnsCreateFailure,
+                "User is not authorized"
+            );
+            return;
+        }
+        const newColumn = new ColumnModel({
+            title: data.title,
+            boardId: data.boardId,
+            userId: socket.user.id,
+        });
+        const savedColumn = await newColumn.save();
+        io.to(data.boardId).emit(
+            SocketEventsEnum.columnsCreateSuccess,
+            savedColumn
+        );
+        console.log("savedColumn", savedColumn);
+    } catch (err) {
+        socket.emit(
+            SocketEventsEnum.columnsCreateFailure,
+            getErrorMessage(err)
+        );
     }
 };
