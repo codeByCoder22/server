9c2d9678e038d159a6bd026116715b467c538d75
WuQiang <wuqiang5733@gmail.com>
2023-05-14 20:11:58 +0930

    Update column

--- src/server.ts
+++ src/server.ts
@@ -97,6 +97,9 @@ io.use(async (socket: Socket, next) => {
     socket.on(SocketEventsEnum.columnsDelete, (data) => {
         columnsController.deleteColumn(io, socket, data);
     });
+    socket.on(SocketEventsEnum.columnsUpdate, (data) => {
+        columnsController.updateColumn(io, socket, data);
+    });
 });
 
 mongoose.connect("mongodb://127.0.0.1:27017/eltrello").then(() => {
